4cfc98974d606ac8764c743494a924875bbbfe4a
DreynPlay <dreynplay@gmail.com>
2024-11-05 17:59:43 +0500

    Task 1 Solved

---
+++ MaximTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using MaximTasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Ваша строка пустая");
+            return;
+        }
+
+        string result = Utility.ProcessString(input);
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
---
+++ MaximTasks/Utility.cs
@@ -0,0 +1,20 @@
+﻿namespace MaximTasks;
+
+public class Utility
+{
+    public static string ProcessString(string input)
+    {
+        if (input.Length % 2 == 0)
+        {
+            int midIndex = input.Length / 2;
+            return ReverseString(input.Substring(0, midIndex)) + ReverseString(input.Substring(midIndex));
+        }
+        return ReverseString(input) + input;
+    }
+    public static string ReverseString(string str)
+    {
+        var charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
